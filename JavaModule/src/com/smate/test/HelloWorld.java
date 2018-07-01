48c4530770b4dc7d0896675f6440fe055884700a
aijiangbin <aijiangbin-computer@163.com>
2018-07-01 14:05:57 +0800

    第一次提交文件

--- JavaModule/src/com/smate/test/HelloWorld.java
+++ JavaModule/src/com/smate/test/HelloWorld.java
@@ -7,4 +7,7 @@ public class HelloWorld {
     public static void  main(String[] args){
         System.out.print("44444");
     }
+    public   static  void  add(String s , String  s2){
+        System.out.print(s+s2);
+    }
 }
--- JavaModule/src/com/smate/test/StringTest.java
+++ JavaModule/src/com/smate/test/StringTest.java
@@ -4,4 +4,8 @@ package com.smate.test;
  * Greate at by ${user}  on ${date}
  */
 public class StringTest {
+    public  static void  main (String[] args){
+
+    }
+
 }
