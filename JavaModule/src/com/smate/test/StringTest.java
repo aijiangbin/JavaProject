ac57bff64fe0ffedab2965aafd75ec21ccfac8da
aijiangbin <aijiangbin-computer@163.com>
2018-07-01 10:57:21 +0800

    测试idea 上传一个新的项目

---
+++ JavaModule/src/com/smate/test/StringTest.java
@@ -0,0 +1,7 @@
+package com.smate.test;
+
+/**
+ * Greate at by ${user}  on ${date}
+ */
+public class StringTest {
+}
---
+++ JavaModule/src/com/smate/test/Test.java
@@ -0,0 +1,7 @@
+package com.smate.test;
+
+/**
+ * Greate at by aijiangbin  on 2018/7/1
+ */
+public class Test {
+}
